4b75cda38465c62f7e9426288107382ffd3a14d1
SfymmiK <84783879+SFYMMIK@users.noreply.github.com>
2024-11-23 14:20:00 +0000

    almost ready for normal use fixes + images added

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault();
-      const targetID = this.getAttribute('href').substring(1);
-      const targetSection = document.getElementById(targetID);
-  
-      window.scrollTo({
-        top: targetSection.offsetTop - 70, // Adjust for fixed navbar
-        behavior: 'smooth',
-      });
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetID = this.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetID);
+
+    window.scrollTo({
+      top: targetSection.offsetTop - 70, // Adjust for fixed navbar
+      behavior: 'smooth',
     });
   });
-  2
+});
